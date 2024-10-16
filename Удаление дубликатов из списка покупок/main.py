eac5b4548567c0dc0b5abe8ef2973f9c1ebbd365
drus454 <kotfatfatfak@gmail.com>
2024-10-17 00:15:54 +0300

    Практические занятия

---
+++ Удаление дубликатов из списка покупок/main.py
@@ -0,0 +1,6 @@
+shopping_list = ["яблоко", "молоко", "хлеб", "яблоко", "масло", "яйца", "молоко"]
+
+unique_items = set(shopping_list)
+sat = len(unique_items)
+
+print("Количество уникальных товаров:", sat)
